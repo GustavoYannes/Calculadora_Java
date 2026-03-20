fccdc37dda3c88879ba0e13a6d2f5b989b7892c6
Gustavo Yannes <yannesgustavo@gmail.com>
2026-03-20 18:50:20 -0300

    Inicio do projeto

---
+++ App.java
@@ -0,0 +1,10 @@
+package Calculadora_Java;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo à Calculadora Java!");
+        // Aqui você pode adicionar código para interagir com a calculadora
+    }
+    
+}
---
+++ Calculadora.java
@@ -0,0 +1,23 @@
+package Calculadora_Java;
+
+public class Calculadora {
+    public double resultado;
+    public boolean validarOperacao(String valor1, String operador, String valor2){
+        return true;
+
+
+    }
+    public String separarString(String expressao){
+        return valor1 + " " + operador + " " + valor2;
+    }
+
+    public double calcular(String valor1, String operador, String valor2){
+       return resultado; 
+    }
+
+    public double converterValor(String valor){
+        return Double.parseDouble(valor);
+    }
+
+
+}

6c43bf466fd966274ae3dae1d2cf5286aa831b56
Gustavo Yannes <yannesgustavo@gmail.com>
2026-03-21 15:59:00 -0300

    Código base pronto

--- App.java
+++ App.java
@@ -3,8 +3,12 @@ package Calculadora_Java;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Bem-vindo à Calculadora Java!");
-        // Aqui você pode adicionar código para interagir com a calculadora
+       IO.println("Calcadora Java");
+       String equacao = IO.readln("Digite a expressão (ex: valor operador valor): ");
+         Calculadora calculadora = new Calculadora(equacao);
+         String resultado = calculadora.resultado + "";
+            IO.println("Resultado: " + resultado);
+        
     }
     
 }
--- Calculadora.java
+++ Calculadora.java
@@ -1,23 +1,73 @@
 package Calculadora_Java;
 
 public class Calculadora {
+    public double valor1;
+    public double valor2;
+    public String operador;
     public double resultado;
-    public boolean validarOperacao(String valor1, String operador, String valor2){
-        return true;
 
+    public Calculadora(String equacao) {
+        separarString(equacao);
+        tipoOperacao();
+        chamarOperacao();
+        }
 
+    public String tipoOperacao(){
+        return switch (operador) {
+            case "+" -> "Soma";
+            case "-" -> "Subtração";
+            case "*" -> "Multiplicação";
+            case "/" -> "Divisão";
+            default -> "Operação inválida";
+        };
     }
-    public String separarString(String expressao){
+
+    public double chamarOperacao (){
+            return resultado = switch (tipoOperacao()) {
+            case "Soma" -> somar();
+            case "Subtração" -> subtrair();
+            case "Multiplicação" -> multiplicar();
+            case "Divisão" -> dividir();
+            default -> 0d;
+        };
+    }
+
+    public String separarString(String equacao) {
+        String[] partes = equacao.split(" ");
+        String valor1 = partes[0];
+        this.valor1 = converterValor(valor1);
+        String operador = partes[1];
+        String valor2 = partes[2];
+        this.valor2 = converterValor(valor2);
+        this.operador = operador;
         return valor1 + " " + operador + " " + valor2;
     }
 
-    public double calcular(String valor1, String operador, String valor2){
-       return resultado; 
-    }
 
-    public double converterValor(String valor){
+    public double converterValor(String valor) {
         return Double.parseDouble(valor);
     }
 
+   public double somar(){
+        resultado = valor1 + valor2;
+        return resultado;   
+   }
 
+    public double subtrair(){
+        resultado = valor1 - valor2;
+        return resultado;
+    }
+
+    public double multiplicar(){
+        resultado = valor1 * valor2;
+        return resultado;
+    }   
+
+    public double dividir(){
+        if(valor2 == 0){
+            return 0;
+        }
+        resultado = valor1 / valor2;
+        return resultado;
+    }
 }
